fb239b2d0f05c6c3e5848ca8c58d506c70d77e7d
Rob Manthey <ribman@gmail.com>
2019-10-06 00:23:27 +1000

    make calls case insensitive

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ async def on_message(message):
         return
 
 # -------------------------------------------------------
-    if (message.content.startswith('hello') or message.content.startswith('hey') or  message.content.startswith('hi')):
+    if (message.content.lower().startswith('hello') or message.content.lower().startswith('hey') or  message.content.lower().startswith('hi')):
         print(f"Saying hi to {message.author.name}")
         msg = f"Hello! {message.author.mention}"
         #msg = 'Hello! {0.author.mention}'.format(message)
@@ -36,7 +36,7 @@ async def on_message(message):
         await client.send_message(message.channel, embed=embed)
 
 # -------------------------------------------------------
-    if (message.content.startswith('c.pic') or message.content.startswith('c.picture')):
+    if (message.content.lower().startswith('c.pic') or message.content.lower().startswith('c.picture')):
         print(f"Sending {message.author.name} a cat pic")
         embed = discord.Embed(title="(✿◕‿◠)~★", description="Here is a cute cat for you~!", color=0x4c8cd6)
         embed.set_image(url="https://images.unsplash.com/photo-1532386236358-a33d8a9434e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60")
@@ -45,7 +45,7 @@ async def on_message(message):
         await client.send_message(message.channel, embed=embed)
 
 # -------------------------------------------------------
-    if (message.content.startswith('c.info') or message.content.startswith('c.botinfo')):
+    if (message.content.lower().startswith('c.info') or message.content.lower().startswith('c.botinfo')):
         print(f"telling {message.author.name} info about catbot")
         embed = discord.Embed(title="(✿◕‿◠)~★", description="catbot is a basic bot with cute mini games where you can collect cats (=ᵔ ﻌ ᵔ=)ﾉ", color=0x4c8cd6)
         embed.add_field(name="Authors", value="@joclaire2#5534 (bot design) and @ribman#7979 (bot coding)")
