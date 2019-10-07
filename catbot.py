dd06407df714c661b686646a0db413a213b62946
robmanthey <ribman@gmail.com>
2019-10-07 21:53:17 +1000

    placing text_face in function

--- catbot.py
+++ catbot.py
@@ -31,19 +31,20 @@ async def on_message(message):
     msgText = message.content.lower()
     msgrName = msgAuthor.name
     msgChannel = message.channel
+    embedColor = 0x4c8cd6
 
 # -------------------------------------------------------
     if (msgText.startswith('hello') or msgText.startswith('hey') or  msgText.startswith('hi')):
         print(f"Saying hi to {msgrName}")
         msg = f"Hello! {msgAuthor.mention}"
-        embed = discord.Embed(title="(✿◕‿◠)~★", description=msg, color=0x4c8cd6)
+        embed = discord.Embed(title=get_text_face(), description=msg, color=embedColor)
         print(embed)
         await msgChannel.send(embed=embed)
 
 # -------------------------------------------------------
     if (msgText.startswith('c.pic') or msgText.startswith('c.picture')):
         print(f"Sending {msgrName} a cat pic")
-        embed = discord.Embed(title="(づ｡◕‿‿◕｡)づ", description="Here is a cute cat for you~!", color=0x4c8cd6)
+        embed = discord.Embed(title=get_text_face(), description="Here is a cute cat for you~!", color=embedColor)
         embed.set_image(url="https://images.unsplash.com/photo-1532386236358-a33d8a9434e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60")
         embed.add_field(name="Credit", value="Photo by Raul Varzar https://unsplash.com/@calypso999 on Unsplash https://unsplash.com/")
         print(embed)
@@ -52,12 +53,16 @@ async def on_message(message):
 # -------------------------------------------------------
     if (msgText.startswith('c.info') or msgText.startswith('c.botinfo')):
         print(f"telling {msgrName} info about catbot")
-        embed = discord.Embed(title="(✿◕‿◠)~★", description="catbot is a basic bot with cute mini games where you can collect cats (=ᵔ ﻌ ᵔ=)ﾉ", color=0x4c8cd6)
+        embed = discord.Embed(title=get_text_face(), description="catbot is a basic bot with cute mini games where you can collect cats (=ᵔ ﻌ ᵔ=)ﾉ", color=embedColor)
         embed.add_field(name="Authors", value="@joclaire2#5534 (bot design) and @ribman#7979 (bot coding)")
         embed.add_field(name="Invite link", value="[Click here to invite catbot to your server!](<https://discordapp.com/api/oauth2/authorize?client_id=625644432741629992&permissions=388160&scope=bot>)")
         embed.add_field(name="Official catbot support server", value="[Click here to join our server!](<https://discord.gg/G6A4VEa>)")
         print(embed)
         await msgChannel.send(embed=embed)
+
+# =======================================================
+def get_text_face():
+    return "(✿◕‿◠)~★"
 
 # =======================================================
 # Create the bot
