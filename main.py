d47261b7e24fd787cc8838a7a4f5aed7fd8c0307
robmanthey <ribman@gmail.com>
2019-10-07 16:52:15 +1000

    tidy up main.py for self-update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# catbot.py
+# catbot main.py
 # version 0.2.0+
 
 import os
@@ -8,18 +8,12 @@ os.system('rm -fr ./catbot')
 # initial setup. re-use if entire repo blown away
 os.system('git clone https://github.com/joclaire2/catbot')
 
+os.system('cp ./catbot/main.py ./')
+os.chdir('./catbot')
+
 # clean up local checkout if anyone's been tinkering
 # os.system('git reset --hard')
-
-os.system('pwd')
-os.system('ls -altr')
-
-os.chdir('./catbot')
-os.system('pwd')
-os.system('ls -altr')
-
-os.system('git status')
-#os.system('git pull')
-# os.system('git status')
+# softer option to cloning
+# os.system('git pull')
 
 os.system('python catbot.py')
